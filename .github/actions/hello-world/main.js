2219541046bccfe6f483efe49a6414ff634aa49a
Yadhav Jayaraman <57544838+decyjphr@users.noreply.github.com>
2020-05-13 12:24:31 -0400

    Update main.js

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -6,7 +6,7 @@ async function run() {
     const token = core.getInput("repo-token");
     const octokit = new github.GitHub(token);
     //const issueComment = octokit.issue({ body: 'Hello World!' })
-    const newIssueComment = await octokit.issues.createComment({ 'issue_number': github.context.issue.number, body: 'Hello World!' })
+    const newIssueComment = await octokit.issues.createComment({ owner: github.context.repo.owner, repo: github.context.repo.repo, issue_number: github.context.issue.number, body: 'Hello World!' })
   } catch (error) {
     core.setFailed(error.message);
   }
