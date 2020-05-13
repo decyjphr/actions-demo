299eebc08f0e354ffc69462da37d3e10b8071fa3
Yadhav Jayaraman <decyjphr@Yadhavs-MacBook-Pro.local>
2020-05-13 12:02:11 -0400

    add comment

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  try {
+    const octokit = new github.GitHub(github.token);
+    const issueComment = octokit.issue({ body: 'Hello World!' })
+    context.github.issues.createComment(issueComment)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
